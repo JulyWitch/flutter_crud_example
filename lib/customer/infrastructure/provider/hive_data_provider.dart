1cc50362e13ee593f9e2477a7fb012e4057e7884
Sajad Abdollahi <sajad.abdollahi1380@gmail.com>
2023-04-15 12:36:41 +0430

    feat: adding delay to hive operations since it's really fast

--- lib/customer/infrastructure/provider/hive_data_provider.dart
+++ lib/customer/infrastructure/provider/hive_data_provider.dart
@@ -17,36 +17,49 @@ class HiveDataProvider {
     box = await Hive.openBox<CustomerEntity>(_boxName);
   }
 
-  Future<void> add(CustomerEntity model) {
+  Future<void> add(CustomerEntity model) async {
     final id = uuid.v4();
 
     final newModel = model.copyWith(id: id);
+    await Future.delayed(const Duration(milliseconds: 500));
 
     return box.put(id, newModel);
   }
 
-  Future<void> update(CustomerEntity model) {
+  Future<void> update(CustomerEntity model) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return box.put(model.id, model);
   }
 
-  Future<void> delete(String id) {
+  Future<void> delete(String id) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return box.delete(id);
   }
 
-  CustomerEntity? get(String id) {
+  Future<CustomerEntity?> get(String id) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return box.get(id);
   }
 
   Future<List<CustomerEntity>> getAll() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return box.values.toList();
   }
 
-  bool isEmailAvailable(String email) {
+  Future<bool> isEmailAvailable(String email) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return !box.values.any((element) => element.email != email);
   }
 
-  bool isFirstNameLastNameBirthDateAvailable(
-      String firstName, String lastName, DateTime dateOfBirth) {
+  Future<bool> isFirstNameLastNameBirthDateAvailable(
+      String firstName, String lastName, DateTime dateOfBirth) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
     return !box.values.any(
       (element) =>
           element.firstName == firstName &&
